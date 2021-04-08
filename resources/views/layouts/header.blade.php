091edb5b14edb99d50f82a2de167a332e47d986d
Jacob Hushaw <Jshushaw@gmail.com>
2021-04-08 16:44:24 -0700

    fixed navbar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,11 +7,11 @@
                 <ul class="nav navbar-nav mr-auto">
 <!-- add if statement, check session and hide if not logged in -->
                 @if (Session::has('User'))   
-                    <li class="nav-item"><a class="nav-link" href="Calendar">Calendar</a></li>
-                    <li class="nav-item"><a class="nav-link" href="Events">Events</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/CCCEventManager/Calendar">Calendar</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/CCCEventManager/Events">Events</a></li>
                     @endif       
                     @if (Session::has('User') && Session::get('Admin') == 1)
-                    <li class="nav-item"><a class="nav-link" href="AddEvent">Add Events</a></li>                                                    
+                    <li class="nav-item"><a class="nav-link" href="/CCCEventManager/AddEvent">Add Events</a></li>                                                    
                     <!-- <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Events</a>
                         <div class="dropdown-menu"><a class="dropdown-item" href="#">First Item</a><a class="dropdown-item" href="#">Second Item</a><a class="dropdown-item" href="#">Third Item</a></div>
                     </li> -->
@@ -19,10 +19,10 @@
                 </ul>
                 <span class="navbar-text actions"> 
                 @if (Session::has('User'))
-                    <a class="btn btn-dark action-button" role="button" href="Logout">Log Off</a> 
+                    <a class="btn btn-dark action-button" role="button" href="/CCCEventManager/Logout">Log Off</a> 
                 @else                                                           
-                	<a class="login" href="Login">Log In</a>
-                	<a class="btn btn-light action-button" role="button" href="Register">Sign Up</a>
+                	<a class="login" href="/CCCEventManager/Login">Log In</a>
+                	<a class="btn btn-light action-button" role="button" href="/CCCEventManager/Register">Sign Up</a>
                 @endif
                 </span>
             </div>
